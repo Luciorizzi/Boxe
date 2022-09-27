1c9c14b481c45bc6596ba53336ae5a83cef5f706
Lucio Rizzi <rizzilucio25@gmail.com>
2022-09-27 17:28:18 -0300

    Hipervinculos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Forms from "./Components/Forms";
+import { IonIcon } from "react-ion-icon";
 function App() {
   return (
     <html className="App">
@@ -18,23 +19,18 @@ function App() {
     <div class="wave" id="wave4"></div>
   </div>
   <ul class="social_icon">
-    <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
-    <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
-    <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
-    <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+
+    <li><a href="mailto:soporteboxe@gmail.com"><img src="https://i.imgur.com/zSww71r.png?1"></img></a></li>
+    <li><a href="https://www.instagram.com/soporteboxe/"><img src="https://i.imgur.com/oBYoLfY.png?1" ></img></a></li>
+    <li><a href="https://wa.me/5491173603596"> <img src="https://i.imgur.com/4gVSUZz.png?1"></img></a></li>
   </ul>
   
-  <ul class="menu">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Services</a></li>
-    <li><a href="#">Team</a></li>
-    <li><a href="#">Contact</a></li>
-  </ul>
 </footer>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
     </html>
+    
   );
 }
 
