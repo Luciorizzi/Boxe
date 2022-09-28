c7efe35003a45dea4ae24b903144b6d8a7845d1d
Lucio Rizzi <rizzilucio25@gmail.com>
2022-09-28 13:48:39 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
 
     <div className="App">
+      <title>Boxe</title>
       <header className="App-header">
         <img src="https://i.imgur.com/GcVQ81Y.jpg?1" alt="logo" />
         <h1>Formulario de contacto</h1>
@@ -12,61 +13,29 @@ function App() {
         <Forms />
       </div>
       <footer>
-<<<<<<< HEAD
-        <div className="waves">
-          <div className="wave" id="wave1"></div>
-          <div className="wave" id="wave2"></div>
-          <div className="wave" id="wave3"></div>
-          <div className="wave" id="wave4"></div>
-        </div>
-        <ul className="social_icon">
-          <li>
-            <a href="mailto:soporteboxe@gmail.com">
-              <img src="https://i.imgur.com/zSww71r.png?1" alt="email"></img>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/soporteboxe/">
-              <img
-                src="https://i.imgur.com/oBYoLfY.png?1"
-                alt="instagram"
-              ></img>
-            </a>
-          </li>
-          <li>
-            <a href="https://wa.me/5491173603596">
-              {" "}
-              <img src="https://i.imgur.com/4gVSUZz.png?1" alt="whatsapp"></img>
-            </a>
-          </li>
-        </ul>
-      </footer>
-     
-    </div>
-=======
+
   <div class="waves">
     <div class="wave" id="wave1"></div>
     <div class="wave" id="wave2"></div>
     <div class="wave" id="wave3"></div>
     <div class="wave" id="wave4"></div>
-  </div>
+    </div>
 
-  <img  className="boxe_footer" src="https://i.imgur.com/yGBBMAI.jpg"/>
+  <img  className="boxe_footer" src="https://i.imgur.com/yGBBMAI.jpg" alt="logoboxe"/>
 
   <ul class="social_icon">
 
-    <li><a href="mailto:soporteboxe@gmail.com"><img src="https://i.imgur.com/zSww71r.png?1" alt="email"></img></a></li>
-    <li><a href="https://www.instagram.com/soporteboxe/"><img src="https://i.imgur.com/oBYoLfY.png?1" alt="instagram"></img></a></li>
-    <li><a href="https://wa.me/5491173603596"> <img src="https://i.imgur.com/4gVSUZz.png?1" alt="whatsapp"></img></a></li>
+    <li><a href="mailto:soporteboxe@gmail.com"><img src="https://i.imgur.com/RbqiWlI.png" alt="email"></img></a></li>
+    <li><a href="https://www.instagram.com/soporteboxe/"><img src="https://i.imgur.com/iYi42Mj.png" alt="instagram"></img></a></li>
+    <li><a href="https://wa.me/5491173603596"> <img src="https://i.imgur.com/q58fuw2.png" alt="whatsapp"></img></a></li>
   </ul>
   
 </footer>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
-    </html>
-    
->>>>>>> 2fb6e441c11f873c7613fb75e097a29cd705a289
+</div>
+
   );
 }
 
