2167ad37060dd19b1227eda690bc28325c39232f
Lucio Rizzi <rizzilucio25@gmail.com>
2022-09-28 13:05:43 -0300

    footerboxe

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import Forms from "./Components/Forms";
 function App() {
   return (
-    <html className="App">
+
+    <div className="App">
       <header className="App-header">
         <img src="https://i.imgur.com/GcVQ81Y.jpg?1" alt="logo" />
         <h1>Formulario de contacto</h1>
@@ -11,25 +12,36 @@ function App() {
         <Forms />
       </div>
       <footer>
-  <div class="waves">
-    <div class="wave" id="wave1"></div>
-    <div class="wave" id="wave2"></div>
-    <div class="wave" id="wave3"></div>
-    <div class="wave" id="wave4"></div>
-  </div>
-  <ul class="social_icon">
-
-    <li><a href="mailto:soporteboxe@gmail.com"><img src="https://i.imgur.com/zSww71r.png?1" alt="email"></img></a></li>
-    <li><a href="https://www.instagram.com/soporteboxe/"><img src="https://i.imgur.com/oBYoLfY.png?1" alt="instagram"></img></a></li>
-    <li><a href="https://wa.me/5491173603596"> <img src="https://i.imgur.com/4gVSUZz.png?1" alt="whatsapp"></img></a></li>
-  </ul>
-  
-</footer>
-<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
-    </html>
-    
+        <div className="waves">
+          <div className="wave" id="wave1"></div>
+          <div className="wave" id="wave2"></div>
+          <div className="wave" id="wave3"></div>
+          <div className="wave" id="wave4"></div>
+        </div>
+        <ul className="social_icon">
+          <li>
+            <a href="mailto:soporteboxe@gmail.com">
+              <img src="https://i.imgur.com/zSww71r.png?1" alt="email"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/soporteboxe/">
+              <img
+                src="https://i.imgur.com/oBYoLfY.png?1"
+                alt="instagram"
+              ></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://wa.me/5491173603596">
+              {" "}
+              <img src="https://i.imgur.com/4gVSUZz.png?1" alt="whatsapp"></img>
+            </a>
+          </li>
+        </ul>
+      </footer>
+     
+    </div>
   );
 }
 
--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -1,30 +1,64 @@
 import "./Forms.css";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import { useRef } from "react";
 
-
 const Forms = () => {
-    const form = useRef();
-  
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm("service_w8664bn","template_gycnflk", form.current, "Xco0YNpWvxczvfEmS")
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
-  
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_w8664bn",
+        "template_gycnflk",
+        form.current,
+        "Xco0YNpWvxczvfEmS"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <form className="form_container" ref={form} onSubmit={sendEmail}>
-      <input className="form_item" type="name" placeholder="Nombre completo" name="name"></input>
-      <input className="form_item" type="tel" placeholder="Numero 011 5555-5555 " name="location"></input>
-      <input className="form_item" type="email" placeholder="ejemplo@ejemplo.com" name="email" ></input>
-      <input className="form_item" type="text" placeholder="Localidad" name="num"></input>
-      <textarea rows="10" cols="80" className="form_textarea" placeholder=" Motivo de contacto" name="motive"></textarea>
-        <button className="form_button">Enviar</button>
+      <input
+        className="form_item"
+        type="name"
+        placeholder="Nombre completo"
+        name="name"
+      ></input>
+      <input
+        className="form_item"
+        type="tel"
+        placeholder="Numero 011 5555-5555 "
+        name="location"
+      ></input>
+      <input
+        className="form_item"
+        type="email"
+        placeholder="ejemplo@ejemplo.com"
+        name="email"
+      ></input>
+      <input
+        className="form_item"
+        type="text"
+        placeholder="Localidad"
+        name="num"
+      ></input>
+      <textarea
+        rows="10"
+        cols="80"
+        className="form_textarea"
+        placeholder=" Motivo de contacto"
+        name="motive"
+      ></textarea>
+      <button className="form_button">Enviar</button>
     </form>
   );
 };
