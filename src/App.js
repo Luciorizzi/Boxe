f918df8b9c993e8ab8cad5ac08a7eca6129dc166
Lucio Rizzi <rizzilucio25@gmail.com>
2022-09-30 11:05:49 -0300

    Pulido

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Forms from "./Components/Forms";
 function App() {
   return (
-
     <div className="App">
       <title>Boxe</title>
       <header className="App-header">
@@ -13,29 +12,39 @@ function App() {
         <Forms />
       </div>
       <footer>
+        <div className="waves">
+          <div className="wave" id="wave1"></div>
+          <div className="wave" id="wave2"></div>
+          <div className="wave" id="wave3"></div>
+          <div className="wave" id="wave4"></div>
+        </div>
 
-  <div class="waves">
-    <div class="wave" id="wave1"></div>
-    <div class="wave" id="wave2"></div>
-    <div class="wave" id="wave3"></div>
-    <div class="wave" id="wave4"></div>
+        <img
+          className="boxe_footer"
+          src="https://i.imgur.com/yGBBMAI.jpg"
+          alt="logoboxe"
+        />
+
+        <ul className="social_icon">
+          <li>
+            <a href="mailto:soporteboxe@gmail.com">
+              <img src="https://i.imgur.com/RbqiWlI.png" alt="email"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/soporteboxe/">
+              <img src="https://i.imgur.com/iYi42Mj.png" alt="instagram"></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://wa.me/5491173603596">
+              {" "}
+              <img src="https://i.imgur.com/q58fuw2.png" alt="whatsapp"></img>
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
-
-  <img  className="boxe_footer" src="https://i.imgur.com/yGBBMAI.jpg" alt="logoboxe"/>
-
-  <ul class="social_icon">
-
-    <li><a href="mailto:soporteboxe@gmail.com"><img src="https://i.imgur.com/RbqiWlI.png" alt="email"></img></a></li>
-    <li><a href="https://www.instagram.com/soporteboxe/"><img src="https://i.imgur.com/iYi42Mj.png" alt="instagram"></img></a></li>
-    <li><a href="https://wa.me/5491173603596"> <img src="https://i.imgur.com/q58fuw2.png" alt="whatsapp"></img></a></li>
-  </ul>
-  
-</footer>
-<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
-</div>
-
   );
 }
 
